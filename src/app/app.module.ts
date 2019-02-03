c14ceeae6e0bb26b17fff15abecb49945c588451
David Dale <ddale@corporatespending.com>
2019-02-02 21:01:00 -0500

    first checking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Media } from '@ionic-native/media/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+  	Media,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+	private songMedia: MediaObject;
+
+	constructor(private mediaPlugin: Media){}
+	
+	playSong(){
+	console.log("play song");
+		this.songMedia = this.mediaPlugin.create( "https://s3.us-east-2.amazonaws.com/drpc/BillHouston/1985-08-25-Pentecost.flac" );
+		this.songMedia.play();
+	}
+
+	pauseSong(){
+	console.log("pause song");
+		this.songMedia.pause();
+	}
+
+	stopSong(){
+	console.log("stop song");
+		this.songMedia.stop();
+		this.songMedia.release();
+	}
+
+
 
 }
